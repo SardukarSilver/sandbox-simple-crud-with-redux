930395b3070b2a200e1b47ea87b2c935eefd795e
Nikita Chotiy <sardukar.silver@gmail.com>
2020-03-09 18:04:45 +0200

    Updates

---
+++ precache-manifest.8c60c68c0df9f6c8555cd36a7ca5ad60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf25d782b6d1fe0aa8c1e222f2a0fc4",
+    "url": "/sandbox-simple-react-crud-with-redux/index.html"
+  },
+  {
+    "revision": "6fd4ecc4322c54f6a6a4",
+    "url": "/sandbox-simple-react-crud-with-redux/static/css/main.b8a51f01.chunk.css"
+  },
+  {
+    "revision": "240a2e5a3f704b33d1c1",
+    "url": "/sandbox-simple-react-crud-with-redux/static/js/2.4c86209c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/sandbox-simple-react-crud-with-redux/static/js/2.4c86209c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd4ecc4322c54f6a6a4",
+    "url": "/sandbox-simple-react-crud-with-redux/static/js/main.e93b3803.chunk.js"
+  },
+  {
+    "revision": "bc604ee04c757b49d1d4",
+    "url": "/sandbox-simple-react-crud-with-redux/static/js/runtime-main.a9cf0b02.js"
+  }
+]);
